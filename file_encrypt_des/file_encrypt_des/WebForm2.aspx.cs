9c057e9b3412fba63ef0d8766d6721a73f61d3b9
susmitha <sush02a@gmail.com>
2015-06-19 12:04:39 +0530

    Update WebForm2.aspx.cs

--- file_encrypt_des/file_encrypt_des/WebForm2.aspx.cs
+++ file_encrypt_des/file_encrypt_des/WebForm2.aspx.cs
@@ -42,9 +42,7 @@ namespace file_encrypt_des
 
         private void Decrypt(string inputFilePath, string outputfilePath)
         {
-            string EncryptionKey = "
-            
-            ";
+            string EncryptionKey = "MAKV2SPBNI99212";
             using (Aes encryptor = Aes.Create())
             {
                 Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
